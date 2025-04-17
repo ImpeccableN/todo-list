74f3bf45989f309ad8861bf6df724ed1c2f3e488
ImpeccableN <108433588+ImpeccableN@users.noreply.github.com>
2025-04-17 19:25:09 +0200

    worked on createForm function. creates the form to fill out Task Infos

--- src/js/dom.js
+++ src/js/dom.js
@@ -1,14 +1,69 @@
 import { createTodo } from "./todos.js";
 
+const content = document.querySelector("#content");
 
 export const createButton = () => {
-    const content = document.querySelector("#content");
     const newButton = document.createElement("button");
-    newButton.addEventListener("click", () => {
-        alert("create a new todo");
-    });
+    newButton.addEventListener("click", () => {createForm();});
     newButton.textContent = "new ToDo";
     content.appendChild(newButton);
 }
 
+const createForm = () => {
+    const form = document.createElement("form");
 
+    const titleInput = document.createElement("input");
+    const dateInput = document.createElement("input");
+    const descriptionInput = document.createElement("input");
+
+    const prioInput = document.createElement("select");
+    const prioHigh = document.createElement("option");
+    const prioMid = document.createElement("option");
+    const prioLow = document.createElement("option");
+
+    const titleLabel = document.createElement("label");
+    const dateLabel = document.createElement("label");
+    const descriptionLabel = document.createElement("label");
+    const prioLabel = document.createElement("label");
+
+    titleInput.setAttribute("type", "text");
+    titleInput.setAttribute("id", "title");
+    dateInput.setAttribute("type", "date");
+    dateInput.setAttribute("id", "dueDate");
+    descriptionInput.setAttribute("type", "text");
+    descriptionInput.setAttribute("id", "description");
+    prioInput.setAttribute("name", "priority");
+    prioInput.setAttribute("id", "priority");
+    // prioHigh.setAttribute("selected");
+    prioHigh.setAttribute("value", "high");
+    prioMid.setAttribute("value", "mid");
+    prioLow.setAttribute("value", "low");
+    
+    titleLabel.setAttribute("for", "title");
+    dateLabel.setAttribute("for", "dueDate");
+    descriptionLabel.setAttribute("for", "description");
+    prioLabel.setAttribute("for", "priority");
+
+    prioHigh.textContent = "High";
+    prioMid.textContent = "Middle";
+    prioLow.textContent = "Low";
+    
+    titleLabel.textContent = "Task Title:";
+    dateLabel.textContent = "Due Date:";
+    descriptionLabel.textContent = "Task Description:";
+    prioLabel.textContent = "Priority:";
+
+    content.appendChild(form);
+    form.appendChild(titleLabel);
+    form.appendChild(titleInput);
+    form.appendChild(dateLabel);
+    form.appendChild(dateInput);
+    form.appendChild(descriptionLabel);
+    form.appendChild(descriptionInput);
+    form.appendChild(prioLabel);
+    form.appendChild(prioInput);
+    prioInput.appendChild(prioLow);
+    prioInput.appendChild(prioMid);
+    prioInput.appendChild(prioHigh);
+
+}
